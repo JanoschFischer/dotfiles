eeb6ba02739917c10a33836ddaaacd3619163366
Janosch Fischer <janosch-fischer@gmx.de>
2021-01-29 19:47:09 +0100

    added picom changes

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -40,7 +40,7 @@ run pamac-tray &
 run xfce4-power-manager &
 numlockx on &
 blueberry-tray &
-picom --config $HOME/.config/qtile/scripts/picom.conf &
+picom --experimental-backends --config $HOME/.config/qtile/scripts/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 xinput set-prop 14 323 1 &
